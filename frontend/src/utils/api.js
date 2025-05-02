0eb7cb43ccc64827e66577d922c8adfee4bf4a87
gurpreetengineer <gb.gurpreetbamrah@gmail.com>
2025-05-02 19:14:32 +0400

    server path updated

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const initialRoute = '//localhost:3000/api';
+// const initialRoute = '//localhost:3000/api';
+const initialRoute = 'https://vercel-deployment-testing.onrender.come/api';
 
 export const createPost = async (content) => {
   const apiRoute = `${initialRoute}/posts`;
